45de6800b0782cc3d53792240cc127dc4741600d
André Paulino <andrepaulinobtw@gmail.com>
2023-10-21 23:34:04 -0300

    Aula 9 - 10

--- src/com/andrepaulino/gerenciador_cursos/Curso.java
+++ src/com/andrepaulino/gerenciador_cursos/Curso.java
@@ -1,9 +1,11 @@
 package com.andrepaulino.gerenciador_cursos;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Curso {
@@ -12,6 +14,7 @@ public class Curso {
 	private String instrutor;
 	private List<Aula> aulas = new LinkedList<Aula>();
 	private Set<Aluno> alunos = new HashSet<>();
+	private Map<matricula, Aluno> matriculaParaAluno = new HashMap<>();
 
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
@@ -40,6 +43,12 @@ public class Curso {
 
 	public void matricula(Aluno aluno) {
 		this.alunos.add(aluno);
+
+		this.matriculaParaAluno.put(aluno.getNumeroMatricula(), aluno);
+	}
+
+	public Aluno buscaMatriculado(int numero) {
+		return this.matriculaParaAluno.get(numero);
 	}
 
 	public Set<Aluno> getAlunos() {
